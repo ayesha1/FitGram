c2b7d3f0399f1fb094c7af8f65ff6d727c823bce
Ayesha Khan <akhan1231@hotmail.com>
2019-03-24 12:10:25 -0700

    Set up

---
+++ FitGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitGram
+//
+//  Created by Ayesha Khan on 3/24/19.
+//  Copyright © 2019 Ayesha Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
