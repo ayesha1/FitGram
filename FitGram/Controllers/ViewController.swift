a56bf13e64a6a129aeebac82cc3910a94c6ad917
Ayesha Khan <akhan1231@hotmail.com>
2019-05-03 12:54:14 -0700

    stuff I tried

--- FitGram/Controllers/ViewController.swift
+++ FitGram/Controllers/ViewController.swift
@@ -10,32 +10,30 @@ import UIKit
 import CBFlashyTabBarController
 
 class ViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTabBar()
-
     }
-    
-    
     func setupTabBar() {
+
         let exerciseNewsFeedVC = ExerciseNewsFeedViewController()
         exerciseNewsFeedVC.tabBarItem = UITabBarItem(title: "NewsFeed", image: #imageLiteral(resourceName: "dumbbell"), tag: 0)
         let challengeVC = ChallengeViewController()
         challengeVC.tabBarItem = UITabBarItem(title: "Challenge", image: #imageLiteral(resourceName: "lightning-bolt-shadow"), tag: 0)
         let profileVC = ProfileViewController()
         profileVC.tabBarItem = UITabBarItem(title: "Profile", image: #imageLiteral(resourceName: "man-user"), tag: 0)
-        
+
         let tabBarController = CBFlashyTabBarController()
         tabBarController.viewControllers = [exerciseNewsFeedVC, challengeVC, profileVC]
-        //        navigationController?.pushViewController(tabBarController, animated: true)
-        self.navigationController?.pushViewController(tabBarController, animated: true)
-//        self.present(tabBarController, animated: true, completion: nil)
+        navigationController?.pushViewController(tabBarController, animated: true)
+//        let topViewController = UIApplication.shared.keyWindow?.rootViewController
+//        topViewController?.present(tabBarController, animated: true, completion: nil)
+//
+////        self.present(tabBarController, animated: true, completion: nil)
     }
-    
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
+
 }
 
