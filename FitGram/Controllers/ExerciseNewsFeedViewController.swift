80eed7501c02a519f2e1032cdb9f90e2d0ad7f55
Ayesha Khan <akhan1231@hotmail.com>
2019-05-05 22:33:11 -0700

    Add workout Section

--- FitGram/Controllers/ExerciseNewsFeedViewController.swift
+++ FitGram/Controllers/ExerciseNewsFeedViewController.swift
@@ -11,7 +11,8 @@ import SnapKit
 
 class ExerciseNewsFeedViewController: UIViewController {
     var roundButton = UIButton()
-
+    weak var labelMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,12 +20,16 @@ class ExerciseNewsFeedViewController: UIViewController {
         self.view.backgroundColor = UIColor.white
         self.title = "Exercise"
         
+       
+        
         //Button
         self.roundButton = UIButton(type: .custom)
         self.roundButton.setTitleColor(UIColor.orange, for: .normal)
         self.roundButton.addTarget(self, action: #selector(ButtonClick(_:)), for: UIControl.Event.touchUpInside)
         self.view.addSubview(roundButton)
     }
+    
+   
     
     override func viewWillLayoutSubviews() {
         
@@ -44,9 +49,43 @@ class ExerciseNewsFeedViewController: UIViewController {
         
         /** Do whatever you wanna do on button click**/
         print("🤬")
-        
-    }
+        showInputDialog()
 
+    }
+    
+    func showInputDialog() {
+        //Creating UIAlertController and
+        //Setting title and message for the alert dialog
+        let alertController = UIAlertController(title: "Enter Workout", message: "Enter Workout Name, Calories Burned and Time duration", preferredStyle: .alert)
+        
+        //the confirm action taking the inputs
+        let confirmAction = UIAlertAction(title: "Enter", style: .default) { (_) in
+            
+            //getting the input values from user
+            let name = alertController.textFields?[0].text
+            let email = alertController.textFields?[1].text
+            
+            
+        }
+        
+        //the cancel action doing nothing
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+        
+        //adding textfields to our dialog box
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Enter Workout Name"
+        }
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Enter Calories"
+        }
+        
+        //adding the action to dialogbox
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        //finally presenting the dialog box
+        self.present(alertController, animated: true, completion: nil)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
